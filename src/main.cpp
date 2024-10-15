d6b1dec6ae4c59885e4344f497a51c36831ce2d5
pangcrd <ad.hovan1738@gmail.com>
2024-10-15 20:45:38 +0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <encoderRead.h>
+
+EncoderRead Encoder(22, 27, 26);  // GPIO pinA,pinB,Button
+
+void setup() {
+  Serial.begin(115200);
+  Encoder.begin();
+}
+
+void loop() {
+  static int lastCounter = 0;
+  int counter = Encoder.getCounter(); 
+
+  if(counter != lastCounter){
+    Serial.println(counter);
+    lastCounter = counter;
+  }
+
+  if(Encoder.encBtn()) {
+    Serial.println("Button Pressed!");
+  }
+}
